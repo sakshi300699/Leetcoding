d99e648ea9c064d947b4a0bf8ac14d4eb0fadef9
Sakshi Bhandarkar <sakshi300699.b@gmail.com>
2020-11-18 13:11:37 +0530

    Update problem_17.cpp

--- November/problem_17.cpp
+++ November/problem_17.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int mirrorReflection(int p, int q) {
-        int m = 1; // extension count of room
-        int n = 1; // reflection count of laser ray
+        int m = 1; 
+        int n = 1; 
         while(m * p != n * q){
             n++;
             m = n * q / p;

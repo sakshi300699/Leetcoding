660308384cbdca5e6c127025de2ce60186694d3b
Sakshi Bhandarkar <sakshi300699.b@gmail.com>
2020-11-19 22:46:06 +0530

    Update problem_19.cpp

--- November/problem_19.cpp
+++ November/problem_19.cpp
@@ -13,26 +13,23 @@ public:
                 i++;
                 continue;
             }
-            
-            // When we get to closing parenthesis,
-            // we will look back to get the letters to repeat and the number of times
-            // then we replace the original part in the string and place i after that to continue.
+           
             int j = i;
-            while (s[j] != '[') // Extract letters
+            while (s[j] != '[') 
                 j--;
         
             string letters_to_repeat = s.substr(j+1, i-j-1);
             int k = j;
             j--;
-            while ((j > 0) &&(isdigit(s[j]))) // Before opening parethesis we get the number
+            while ((j > 0) &&(isdigit(s[j]))) 
                 j--;
             
-            if (j != 0) j++; // Edge case where we are at the beginning of the string
+            if (j != 0) j++; 
             int times_to_repeat = stoi(s.substr(j, k-j));
             
             s.replace(j, i-j+1, repeat(letters_to_repeat, times_to_repeat));
 			
-            // Put i in the right place now
+            
             i = j+letters_to_repeat.size()*times_to_repeat;
         }
         return s;

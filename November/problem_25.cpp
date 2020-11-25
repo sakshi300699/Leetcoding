27577aa2be29f122a0088d34db9fed7f6bc228ff
Sakshi Bhandarkar <sakshi300699.b@gmail.com>
2020-11-25 20:13:08 +0530

    Update problem_25.cpp

--- November/problem_25.cpp
+++ November/problem_25.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int smallestRepunitDivByK(int K) {
      if(K%2 == 0 || K%5 == 0) return -1;
-      int rez = 1;                           //number of digits 1 in our answer
-      int remainder = 1;               //first remainder
-      int total = 1;                        // summe of our remainders
+      int rez = 1;                          
+      int remainder = 1;              
+      int total = 1;                        
       while(total%K != 0){
         rez++;
         remainder = (remainder*10)%K;

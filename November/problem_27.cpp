99314d09e8c637ef07f4ea920036aa71e091c9cd
Sakshi Bhandarkar <sakshi300699.b@gmail.com>
2020-11-27 21:32:06 +0530

    Update problem_27.cpp

--- November/problem_27.cpp
+++ November/problem_27.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     bool canPartition(vector<int>& nums) {
         int sum = 0;
-        for (auto a : nums) // Sum up the array
+        for (auto a : nums) 
             sum += a;
         
-        if (sum % 2) // If the sum is odd - we can never find two equal partitions
+        if (sum % 2) 
             return false;
         
         sum /= 2;
-        vector<bool> dp(sum+1, false); // dp keeps for each number if it has a subset or not
+        vector<bool> dp(sum+1, false); 
         dp[0] = true;
         
         for (auto a : nums) {
             for (int i = sum; i >= a; i--) {
-                dp[i] = dp[i] || dp[i-a]; // for each number, either we use it or we don't
+                dp[i] = dp[i] || dp[i-a]; 
             } 
         }
         return dp[sum];

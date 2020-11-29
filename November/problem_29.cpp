ba1aba7439cdc6d8633e99a518d21338a1ad88c0
Sakshi Bhandarkar <sakshi300699.b@gmail.com>
2020-11-29 18:51:43 +0530

    Update problem_29.cpp

--- November/problem_29.cpp
+++ November/problem_29.cpp
@@ -11,14 +11,13 @@ public:
         
         int temp = arr[start];
         
-        //To not visit the same position again
-        //Set to some random value and check in the base condition
+        
         arr[start] = -1;
         
         bool ans = canReach(arr, start+temp)
                    || canReach(arr, start-temp);
         
-        //Reset to original Value
+        
         arr[start] = temp;
         
         return ans;

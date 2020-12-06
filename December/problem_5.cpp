239e6dab9abaf4b127f2ce4575de3a97eb6ddb3e
Sakshi Bhandarkar <sakshi300699.b@gmail.com>
2020-12-06 12:44:26 +0530

    Update problem_5.cpp

--- December/problem_5.cpp
+++ December/problem_5.cpp
@@ -9,7 +9,7 @@ public:
         flowerbed.push_back(0);
         flowerbed.insert(flowerbed.begin(), 0);
         
-        // Check if there are three empty plots in a row:
+        
         int flowers = 0, i = 0;
         while ((i < flowerbed.size() - 2) && (flowers < n)) {
             if ((flowerbed[i] == 0) && (flowerbed[i+1] == 0) && (flowerbed[i+2] == 0)) {

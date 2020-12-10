f50379a5e414016f5847ff1f573a40ff01d6b466
Sakshi Bhandarkar <sakshi300699.b@gmail.com>
2020-12-10 23:08:44 +0530

    Update problem_10.cpp

--- December/problem_10.cpp
+++ December/problem_10.cpp
@@ -6,19 +6,19 @@ public:
         
         int i = 0;
         
-        // Go while there's a hill up, untill we reach peak:
+        
         while (i < arr.size()-1 && arr[i] < arr[i+1])
             i++;
            
-        // Peak can't be the first or last:
+        
         if (i == 0 || i == arr.size()-1)
             return false;
         
-        // Walk downhill:
+        
         while (i < arr.size()-1 && arr[i] > arr[i+1])
             i++;
         
-        // If there's a larger number after we started walking down, this will be false:
+        
         return i == arr.size()-1;
     }
 };
